b1394b16e862e08d9dd79f9e915ab1751290692a
Kevin Jin <kevjin2002@gmail.com>
2024-02-20 17:00:14 -0600

    better test case example

--- writingtestcases.py
+++ writingtestcases.py
@@ -8,24 +8,33 @@ Test case format:
         # Assert the expected results
 """
 
-def test_len_of_list():
+# "_<function_name>" denotes private function
+def _check_arrays_are_equal(output: list, expected: list) -> bool:
+    if len(output) != len(expected):
+        return False
+    for i in range(len(output)):
+        if output[i] != expected[i]:
+            return False
+    return True
+
+def test_sorted_int_list_reversed():
     # Set up data
-    test_list = [1, 2, 3, 4, 5]
-    expected_result = 5
+    test_list = [5,3,4,2,1]
+    expected_result = [1,2,3,4,5]
 
     # Call the function
-    result = len(test_list)
+    result = sorted(test_list)
 
     # Assert the expected results
-    assert result == expected_result
+    assert _check_arrays_are_equal(result, expected_result)
 
 """
 In an interview, you can do this more informally
 (no function name, "print" instead of "assert")
 """
 
-test_list = [1, 2, 3, 4, 5]
+test_list = [5,4,3,2,1]
 
-result = len(test_list)
+result = sorted(test_list)
 
 print(result)
